5aaaf86cdc2e3076eed88e41696523588814e3ae
MatheusSPereira <githubmatheus@outlook.com>
2023-08-20 08:15:49 -0300

    first commit

---
+++ jogoadivinhacao.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "**************************************" << endl;
+    cout << "* Bem vindos ao Jogo de adivinhacao! *" << endl;
+    cout << "**************************************" << endl;
+
+    int secret_number = 42;
+    cout << "O numero secreto eh " << secret_number << ". Nao conte para ninguem!" << endl;
+
+    int soma = 5 + 5;
+    cout << "soma eh: " << soma << endl;
+}
